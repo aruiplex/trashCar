8c0a3f066d2617b135d47318fec93368e23ccc7a
aruix <aruix@outlook.com>
2021-07-10 12:50:51 +0800

    chu sai zhi qian

--- show/pi.py
+++ show/pi.py
@@ -1,10 +1,14 @@
 import math
 import serial
+import time
 
 
-port_nano = serial.Serial(port="/dev/ttyAMA1", baudrate=115200)
+# port_nano = serial.Serial(port="/dev/ttyAMA1", baudrate=115200)
 port_arm = serial.Serial(port="/dev/ttyAMA2", baudrate=115200)
-port_wheel = serial.Serial(port="/dev/ttyAMA3", baudrate=115200)
+port_front_left = serial.Serial(port="/dev/ttyAMA1", baudrate=115200)
+port_front_right = serial.Serial(port="/dev/ttyAMA3", baudrate=115200)
+port_back_left = serial.Serial(port="/dev/ttyAMA4", baudrate=115200)
+port_back_right = serial.Serial(port="/dev/ttyAMA0", baudrate=115200)
 
 
 def get_size(a, b):
@@ -21,26 +25,45 @@ def get_size(a, b):
 _size = get_size(20.5, 21.2)
 
 
-def get_mess(i, L):
+def get_mess(L, S):
     """
     Generate the corresponding UART message
-    i is the ID of the motor
     L is the position
+    S is the speed
     """
     m3 = int((L >> 24) & 0xff)  # Position
     m2 = int((L >> 16) & 0xff)
     m1 = int((L >> 8) & 0xff)
     m0 = int(L & 0xff)
-    i_fun = int((i << 4) | 0xB)  # ID information
-    VALUE = [0x48, i_fun, m3, m2, m1, m0, 0xFF, 0xFF, 0xFF, 0xFF]
+    s1 = int((S >> 8) & 0xff)
+    s0 = int(S & 0xff)
+    VALUE = [0xAA, 0x59, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+             0xFF, 0xAA, 0x70, m3, m2, m1, m0, s1, s0, 0xFF, 0xFF]
+    return VALUE
+
+
+def get_pwm(L):
+    """
+    Generate the corresponding UART message
+    i is the ID of the motor
+    L is the speed1
+    """
+
+    m1 = int((L >> 8) & 0xff)  # velocity
+    m0 = int(L & 0xff)
+    VALUE = [0xAA, 0x4E, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+             0xFF, 0xAA, 0x0C, m1, m0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
     return VALUE
 
 # Reset all the wheels (brake instantly)
 
 
 def reset():
-    VALUE = [0x48, 0x05, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-    port_wheel.write(bytearray(VALUE))
+    VALUE = [0xAA, 0x4D, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+    port_back_left.write(bytearray(VALUE))
+    port_back_right.write(bytearray(VALUE))
+    port_front_left.write(bytearray(VALUE))
+    port_front_right.write(bytearray(VALUE))
     return 0
 
 
@@ -55,74 +78,93 @@ def pwm_move(angle, speed):
         print('Error! Speed is too large.')
         return 0
 
-    Sx = speed * math.cos(angle + 3.141592653589/2)
-    Sy = speed * math.sin(angle + 3.141592653589/2)
+    Sx = speed * math.cos(angle + math.pi/2)
+    Sy = speed * math.sin(angle + math.pi/2)
 
     # Calculate the position of each motor
-    S1 = -int(Sy - Sx + 0.5)  # 0.5 is for rounding
-    S2 = int(Sy + Sx + 0.5)
-    S3 = int(Sy - Sx + 0.5)
+    S1 = round(Sy - Sx)  # 0.5 is for rounding
+    S2 = round(Sy + Sx)
+    S3 = round(Sy - Sx)
     # The minus sign for motor 1 and 4 is due to their installation direction
-    S4 = -int(Sy + Sx + 0.5)
+    S4 = round(Sy + Sx)
 
     # Generate the UART messages
-    VALUE1 = get_mess(1, S1)
-    VALUE2 = get_mess(2, S2)
-    VALUE3 = get_mess(3, S3)
-    VALUE4 = get_mess(4, S4)
+    VALUE1 = get_pwm(S1)
+    VALUE2 = get_pwm(S2)
+    VALUE3 = get_pwm(S3)
+    VALUE4 = get_pwm(S4)
 
     # Send messages
-    port_wheel.write(bytearray(VALUE1))
-    port_wheel.write(bytearray(VALUE2))
-    port_wheel.write(bytearray(VALUE3))
-    port_wheel.write(bytearray(VALUE4))
+    port_front_right.write(bytearray(VALUE1))
+    port_front_left.write(bytearray(VALUE2))
+    port_back_left.write(bytearray(VALUE3))
+    port_back_right.write(bytearray(VALUE4))
     print(bytearray(VALUE1))
     print(bytearray(VALUE2))
     print(bytearray(VALUE3))
     print(bytearray(VALUE4))
 
 
-def pid_move(angle, distance, orientation):
+def pid_move(angle, distance, speed, orientation):
     """
     Motion control function
     1. angle is the motion direction angle in radian, 0 at forward direction (posotive y-axis), positive for counter-clockwise
     2. distance is the absolute length for moving. If distance == 0, the car rotates in z-axis
     3. orientation is the angle of the head of the car in radian, same direction with angle
+    4. speed must share the same sign with distance
     """
-    dis = distance * 200 / 25  # Convert the distance into cycles
+    dis = distance * 135 / 25  # Convert the distance into cycles
+    spd = speed * 32767 / 100
 
     if (dis > 2 ** 31) | (dis < -2 ** 31):
         print('Error! Distance is too large.')
         return 0
 
+    if (spd > 32767) | (spd < -32767):
+        print('Error! Speed is too large.')
+        return 0
+
     Lx = dis * math.cos(angle + math.pi/2)
     Ly = dis * math.sin(angle + math.pi/2)
+    Sx = spd * math.cos(angle + math.pi/2)
+    Sy = spd * math.sin(angle + math.pi/2)
 
     # Calculate the position of each motor
-    L1 = -int(Ly - Lx + orientation * _size + 0.5)  # 0.5 is for rounding
+    L1 = int(Ly - Lx + orientation * _size + 0.5)  # 0.5 is for rounding
     L2 = int(Ly + Lx - orientation * _size + 0.5)
     L3 = int(Ly - Lx - orientation * _size + 0.5)
-    # The minus sign for motor 1 and 4 is due to their installation direction
-    L4 = -int(Ly + Lx + orientation * _size + 0.5)
+    L4 = int(Ly + Lx + orientation * _size + 0.5)
+    S1 = int(Sy - Sx + orientation * _size + 0.5)  # 0.5 is for rounding
+    S2 = int(Sy + Sx - orientation * _size + 0.5)
+    S3 = int(Sy - Sx - orientation * _size + 0.5)
+    S4 = int(Sy + Sx + orientation * _size + 0.5)
 
     # Generate the UART messages
-    VALUE1 = get_mess(1, L1)
-    VALUE2 = get_mess(2, L2)
-    VALUE3 = get_mess(3, L3)
-    VALUE4 = get_mess(4, L4)
+    VALUE1 = get_mess(L1, S1)
+    VALUE2 = get_mess(L2, S2)
+    VALUE3 = get_mess(L3, S3)
+    VALUE4 = get_mess(L4, S4)
 
     # Send messages
-    port_wheel.write(bytearray(VALUE1))
-    port_wheel.write(bytearray(VALUE2))
-    port_wheel.write(bytearray(VALUE3))
-    port_wheel.write(bytearray(VALUE4))
+    port_front_right.write(bytearray(VALUE1))
+    port_front_left.write(bytearray(VALUE2))
+    port_back_left.write(bytearray(VALUE3))
+    port_back_right.write(bytearray(VALUE4))
 
 
 def dance():
-    pid_move(0, 500, 0)
-    pid_move(0, -500, 0)
-    pid_move(math.pi/2, 500, 0)
-    pid_move(-math.pi/2, 500, 0)
+    # pwm_move(0, 2000)
+    pid_move(0, 500, 100, 0)
+    time.sleep(1)
+    reset()
+    # time.sleep(1)
+    # reset()
+    # pid_move(math.pi/2, 100, 0)
+    # time.sleep(1)
+    # reset()
+    # pid_move(-math.pi/2, 100, 0)
+    # time.sleep(1)
+    # reset()
 
 
 if __name__ == "__main__":
